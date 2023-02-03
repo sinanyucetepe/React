13a828a107ea0deaf6589ecfdb70c8aa9232dcad
Sinan Yucetepe <sinan.yucetepee@gmail.com>
2023-02-03 16:14:04 +0300

    unique key prop

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
 import User from './componets/User';
 //import Header from './componets/Header'
-
+const friends = [
+  {
+    id: 1,
+  name:'Jack '
+  },
+  {
+    id: 2,
+    name:'Jesse '
+  },
+  {
+    id: 3,
+    name:'Ron '
+  }, 
+];
 function App() {
   return(
     <>
-    <User name="Sinan" surname = "Yucetepe" isLoggedIn= {true} age={26} />
+    <User name="Sinan" surname = "Yucetepe" isLoggedIn= {true} age={26} friends ={ friends} />
 
     
     </>
--- hello-react/src/componets/User.js
+++ hello-react/src/componets/User.js
@@ -1,10 +1,15 @@
-function User({name, surname, isLoggedIn, age}) {
+
+function User({name, surname, isLoggedIn, age, friends}) {
     return (
-    <h1>
-        {
-            isLoggedIn ? `${name} ${surname} (${age})`  : "You did not login"
-        }
-         {isLoggedIn} </h1>
+    <>
+        <h1> 
+        {isLoggedIn ? `${name} ${surname} (${age}) `  : "You did not login"}
+        </h1>
+
+        {friends.map((friend) => 
+            <div key={friend.id}> {friend.name} </div> )
+        }            
+    </>
     );
 }
 export default User;
