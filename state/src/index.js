ee8000e31d23f33e7ea102b31a3a725a630717b8
Sinan Yucetepe <sinan.yucetepee@gmail.com>
2023-02-04 22:57:27 +0300

    using input with states

--- state/src/index.js
+++ state/src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
-import Counter from './componets/Counter';
+// import Counter from './componets/Counter';
+import InputExample from './componets/InputExample';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Counter />
+    <InputExample />
   </React.StrictMode>
 );
 
