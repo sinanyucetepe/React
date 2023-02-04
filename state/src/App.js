be563e0f0b7f166c98477e3fae9bb2ecab73b758
Sinan Yucetepe <sinan.yucetepee@gmail.com>
2023-02-04 16:25:19 +0300

    Array States

--- state/src/App.js
+++ state/src/App.js
@@ -3,12 +3,24 @@ import { useState } from "react";
 function App() {
   const [name, setName] = useState('Sinan');
   const [age, setAge] = useState(26);
+  const [friends, setFriends] = useState( [ "Jack", "Jesse"]);
   return (
     <div className="App">
       <h1>Hello {name}!</h1>
       <h2>{age}</h2>
       <button onClick={() => setName("Tom")}> Click to new name </button>
       <button onClick={() => setAge("20")}> Click to return 20s</button>
+    
+    <hr /> <br /> <br />
+    <h2>Friends</h2>
+    { 
+      friends.map((friend, index) => (
+        <div key={index}> {friend}</div> 
+      ))}
+      
+      <br />
+
+      <button onClick={() => setFriends([ "Jany", ...friends])}> Add a new friend</button> // protect your data previous
     </div>
   );
 }
