c7180263e50fb5334f7f26fe19e10d92ca40cfa9
Sinan Yucetepe <sinan.yucetepee@gmail.com>
2023-02-06 23:59:26 +0300

    Component Unmount

--- use-effect/src/App.js
+++ use-effect/src/App.js
@@ -1,26 +1,15 @@
-import {useState, useEffect} from "react";
+import { useState } from "react";
+import Counter from "./componets/Counter";
 function App() {
-    const [number, setNumber] = useState(0);
-    const [name, setName] = useState("Sinan");
-    useEffect(() => {
-      console.log("number state updated")
-    }, [number])
-    useEffect(() => {
-      console.log("name state updated")
-    }, [name])
+  const [isVisible, setIsVisible]  = useState(true)
+  return (
+    <div className="App">
 
-    useEffect ( () => {
-      console.log(' component mounted')
-    }, [])
-
-    return <div className="App" >
-      <h1>{number}</h1>
-    <button onClick={() => setNumber(number+1)} > Click </button>
-    <hr />
-    <h1>{name}</h1>
-    <button onClick={() => setName("Tom")} > Click a new name </button>
+    <Counter/>
+    <button onClick={()=> setIsVisible(false) }>Toggle</button>
     </div>
- 
+
+  );
 }
 
 export default App;
